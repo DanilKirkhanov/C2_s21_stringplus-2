9bfc51a8e06395d64fa7dda65cffcaa2067a5284
Pinsir Ghezn <pinsirgh@un-n4.kzn.21-school.ru>
2023-06-15 15:43:17 +0300

    memcmp tests added

--- src/string_tests.c
+++ src/string_tests.c
@@ -55,12 +55,49 @@ START_TEST(memcpy_test) {
 
   ck_assert_str_eq(s21_memcpy(destinationOne, source, 14), memcpy(forOriginalOne, source, 14));
   ck_assert_str_eq(s21_memcpy(destinationTwo, source, 7), memcpy(forOriginalTwo, source, 7));
+  ck_assert_str_eq(s21_memcpy(destinationTwo, source, 100), memcpy(forOriginalTwo, source, 100));
   ck_assert_str_eq(s21_memcpy(destinationThree, source, 14), memcpy(forOriginalThree, source, 14));
   ck_assert_str_eq(s21_memcpy(destinationFour, sourceTwo, 15), memcpy(forOriginalFour, sourceTwo, 15));
   ck_assert_str_eq(s21_memcpy(destinationFive, sourceTwo, 15), memcpy(forOriginalFive, sourceTwo, 15));
   ck_assert_str_eq(s21_memcpy(destinationSix, sourceTwo, 5), memcpy(forOriginalSix, sourceTwo, 5));
   ck_assert_str_eq(s21_memcpy(destinationSeven, sourceTwo, 15), memcpy(forOriginalSeven, sourceTwo, 15));
   ck_assert_str_eq(s21_memcpy(destinationEight, sourceTwo, 15), memcpy(forOriginalEight, sourceTwo, 15));
+}
+
+START_TEST(memcmp_test) {
+  char sourceOne[10] = "HELLO";
+  char destinOne[10] = "Hello";
+
+  char sourceTwo[50] = "The first string for testing";
+  char destinTwo[50] = "The second string for testin";
+
+  char sourceThree[50] = "1241613413";
+  char destinThree[50] = "1241613413";
+
+  char sourceFour[50] = "\0";
+  char destinFour[50] = "\0";
+  
+  char sourceFive[50] = "1\n\0";
+  char destinFive[50] = "1\n\0";
+
+  char testOne[50] = "\0";
+  char testTwo[50] = " \n\0";
+  char testThree[50] = "\n\0";
+  char testFour[50] = "b\0";
+  char testSix[50] = "Alpha Chad";
+  char testSeven[50] = "Alpha Chad\0";
+
+  ck_assert_int_eq(s21_memcmp(sourceOne, destinOne, 2), memcmp(sourceOne, destinOne, 2));
+  ck_assert_int_eq(s21_memcmp(sourceTwo, destinTwo, 5), memcmp(sourceTwo, destinTwo, 5));
+  ck_assert_int_eq(s21_memcmp(sourceThree, destinThree, 10), memcmp(sourceThree, destinThree, 10));
+  ck_assert_int_eq(s21_memcmp(sourceFour, destinFour, 1), memcmp(sourceFour, destinFour, 1));
+  ck_assert_int_eq(s21_memcmp(sourceFive, destinFive, 1), memcmp(sourceFive, destinFive, 1));
+  ck_assert_int_eq(s21_memcmp(testOne, "", 1), memcmp(testOne, "", 1));
+  ck_assert_int_eq(s21_memcmp(testTwo, " \n\0", 3), memcmp(testTwo, " \n\0", 3));
+  ck_assert_int_eq(s21_memcmp(testThree, testTwo, 1), memcmp(testThree, testTwo, 1));
+  ck_assert_int_eq(s21_memcmp(testFour, "b\0", 1), memcmp(testFour, "b\0", 1));
+  ck_assert_int_eq(s21_memcmp(testSix, testSeven, 10), memcmp(testSix, testSeven, 10));
+
 }
 
 int main() {
@@ -80,6 +117,12 @@ int main() {
   tc_memcpy = tcase_create("memcpy test");
   suite_add_tcase(s, tc_memcpy);
   tcase_add_test(tc_memcpy, memcpy_test);
+
+// memcmp
+  TCase *tc_memcmp;
+  tc_memcmp = tcase_create("memcmp test");
+  suite_add_tcase(s, tc_memcmp);
+  tcase_add_test(tc_memcmp, memcmp_test);
 
 
 // runner
